7ed6572d88a8447cf2ea3a8bf796c549c4af85f2
content brim <contentbrim@gmail.com>
2025-08-01 16:40:32 +0200

    refactor: unite aeneas and moviepy audio files

--- file.py
+++ file.py
@@ -1,11 +1,20 @@
-from aeneas.audiofile import AudioFile
+from aeneas.audiofile import AudioFile as AeneasAudio
+from moviepy import AudioFileClip as MoviePyAudio
 from aeneas.textfile import TextFile
 
 
-def parse_audio_file(file_path):
-    file = AudioFile(file_path=file_path)
-    file.read_properties()
-    return file
+class AudioFile(AeneasAudio, MoviePyAudio):
+    def __init__(self, file_path=None):
+        AeneasAudio.__init__(self, file_path=file_path)
+        MoviePyAudio.__init__(self, filename=file_path)
+
+    def __enter__(self):
+        self.read_properties()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.clear_data()
+        self.close()
 
 
 def parse_text_file(file_path, file_format="plain"):
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from file import parse_audio_file, parse_text_file
+from file import AudioFile, parse_text_file
 from audio import generate_sync_map
 
 
 if __name__ == "__main__":
     audio_file = input("Enter audio file path: ")
-    audio = parse_audio_file(audio_file)
-
     text_file = input("Enter text file path: ")
-    text = parse_text_file(text_file)
 
-    sync_map = generate_sync_map(audio, text)
+    text = parse_text_file(text_file)
+    with AudioFile(audio_file) as audio:
+        sync_map = generate_sync_map(audio, text)
